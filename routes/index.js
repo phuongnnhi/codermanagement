ba17ead09029337b297f2a5996d489fc08c3b304
phuongnnhi <85156989+phuongnnhi@users.noreply.github.com>
2025-01-10 19:38:03 +0100

    update route

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ const taskRoutes = require('./task.api');
 
 const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Homepage! Please go to /api/users for users api, and /api/tasks for tasks api');
-});
 
 router.use('/api/users', userRoutes);
 router.use('/api/tasks', taskRoutes);
--- server.js
+++ server.js
@@ -13,5 +13,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 .then(()=> console.log('MongoDB connected'))
 .catch(err => console.error(err));
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Homepage! Please go to /api/users for users api, and /api/tasks for tasks api');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
