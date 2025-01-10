86a78be88fcbdbc4eecda8631744d85a657ce89f
phuongnnhi <85156989+phuongnnhi@users.noreply.github.com>
2025-01-10 19:36:04 +0100

    update route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const taskRoutes = require('./task.api');
 
 const router = express.Router();
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Homepage! Please go to /api/users for users api, and /api/tasks for tasks api');
+});
+
 router.use('/api/users', userRoutes);
 router.use('/api/tasks', taskRoutes);
 
